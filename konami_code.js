ca9f87118d4527a815e48c6acb61eaa0717c0f1c
Anthony Lam <al3101@nyu.edu>
2018-05-02 00:35:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,21 +3,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   var index = 0
-  key.addEventListener('keydown', onKeyDownHandler) {
+  key.addEventListener('keydown', onKeyDownHandler);
 
   
 
-function onKeyDownHandler(e) {
-  if(key === code[index]) {
-      index++;
-      if (index === code.length-1) {
-        alert('Congrats!');
+  function onKeyDownHandler(e) {
+    if(key === code[index]) {
+        index++;
+        if (index === code.length-1) {
+          alert('Congrats!');
+          index = 0;
+        }
+      } else { 
         index = 0;
       }
-    } else { 
-      index = 0;
     }
   }
-}
 
 
