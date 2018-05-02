122f42fd71d07afecc13e5f2f13a9e6e9c2dd961
Anthony Lam <al3101@nyu.edu>
2018-05-02 00:35:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const key = document.querySelector('body')
-  key.addEventListener('keydown', onKeyDownHandler(e))
+  var index = 0
+  key.addEventListener('keydown', onKeyDownHandler) {
 
   
 
